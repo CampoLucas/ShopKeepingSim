42bde37c32312322930d17449e2d4b178f8fdbd0
Lu <102192546+CampoLucas@users.noreply.github.com>
2022-12-26 18:33:26 -0300

    Added an inventory ui
    The inventory layour is a list of items with a scroll bar next to it.
    Each item has an icon, their name and the current stock

---
+++ Shop Keeping Sim/Assets/_Main/Scripts/Inventory/IStorable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IStorable
+{
+    ItemSO Data { get; }
+    void Store();
+}
---
+++ Shop Keeping Sim/Assets/_Main/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct InventoryItem
+{
+    public ItemSO Item { get; private set; }
+    public int Stock { get; private set; }
+
+    public InventoryItem(ItemSO item)
+    {
+        Item = item;
+        Stock = 1;
+    }
+
+    public int IncreaseStock() => Stock ++;
+    public int IncreaseStock(int amount) => Stock += amount;
+    public int DecreaseStock() => Stock --;
+    public int DecreaseStock(int amount) => Stock -= amount;
+}
+
+public class Inventory : MonoBehaviour
+{
+    private Dictionary<string, InventoryItem> _itemDictionary;
+    //private List<InventoryItem> _itemsStoredUp;
+    public static Inventory Instance;
+    
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        _itemDictionary = new Dictionary<string, InventoryItem>();
+    }
+
+    public void GetItem(IStorable storableItem)
+    {
+        var data = storableItem.Data;
+        if (_itemDictionary.TryGetValue(data.ID, out var stockItem))
+        {
+            stockItem.IncreaseStock();
+            _itemDictionary[data.ID] = stockItem;
+        }
+        else
+        {
+            _itemDictionary.Add(data.ID, new InventoryItem(data));
+        }
+        
+        //adds it to the ui
+        var newStockItem = _itemDictionary[data.ID];
+        //UIManager.Instance.SetItemButton(newStockItem.Item, newStockItem.Stock);
+    }
+
+    public bool HasItem(IStorable itemToCheck) => _itemDictionary.ContainsKey(itemToCheck.Data.ID);
+    public bool HasItem(string id) => _itemDictionary.ContainsKey(id);
+
+    public int GetItemStock(IStorable item) =>
+        _itemDictionary.TryGetValue(item.Data.ID, out var stock) ? stock.Stock : 0;
+
+    public bool HasItems(List<IStorable> itemsToCheck)
+    {
+        foreach (var itemToCheck in itemsToCheck)
+        {
+            if (!_itemDictionary.ContainsKey(itemToCheck.Data.ID))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    public ItemSO GetItemFromPool(string idToCheck)
+    {
+        if(!_itemDictionary.TryGetValue(idToCheck, out var stockItem)) return null;
+
+        if(stockItem.DecreaseStock() > 0) return stockItem.Item;
+
+        var itemToReturn = stockItem.Item;
+        _itemDictionary.Remove(idToCheck);
+        return itemToReturn;
+    }
+}
